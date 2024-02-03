dbe9d58ebb063f7f4cfa214631fd9ac411296dd1
Archana-Tharammal <126646364+Archana-Tharammal@users.noreply.github.com>
2024-02-03 11:42:00 +0530

    Add files via upload

---
+++ app routing/myproject/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+def home():
+    return "Hello, this is our first flask website";
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
